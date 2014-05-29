ed5e8e828a47cbd0f2d0b1bccf10ad26ee67560a
Cleber Rosa <crosa@redhat.com>
2014-05-28 21:55:48 -0300

    reset-and-run.sh: use port 9405 by default
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- reset-and-run.sh
+++ reset-and-run.sh
@@ -3,4 +3,4 @@ if [ -f avocadoserver.sqlite ]; then rm avocadoserver.sqlite; fi;
 ./scripts/avocado-server-manage syncdb -v0 --noinput
 ./scripts/avocado-server-manage createsuperuser --username=admin --email='root@localhost.localdomain' --noinput
 ./scripts/avocado-server-manage changepassword admin
-./scripts/avocado-server-manage runserver
+./scripts/avocado-server-manage runserver 0.0.0.0:9405
