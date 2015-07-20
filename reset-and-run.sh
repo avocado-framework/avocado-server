00811117c117d18616c83ed691d8d306c100d8f9
Cleber Rosa <crosa@redhat.com>
2015-07-20 16:46:01 -0300

    reset-and-run.sh: simplify steps to get server running
    Since we're not attempting to enforce any distinction between
    regular users and admins, let's drop a few steps from the script.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- reset-and-run.sh
+++ reset-and-run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 if [ -f avocadoserver.sqlite ]; then rm avocadoserver.sqlite; fi;
-./scripts/avocado-server-manage syncdb -v0 --noinput
-./scripts/avocado-server-manage createsuperuser --username=admin --email='root@localhost.localdomain' --noinput
-./scripts/avocado-server-manage changepassword admin
+./scripts/avocado-server-manage migrate
 ./scripts/avocado-server-manage runserver 0.0.0.0:9405
